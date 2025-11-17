5e4ba6354c5123e187a547b5873d6f5bdf8788d1
gangstercoinmine-web <gangstercoin.mine@gmail.com>
2025-11-17 18:29:19 +0530

    Create PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z) * moveSpeed * Time.deltaTime;
+        transform.position += move;
+    }
+}
